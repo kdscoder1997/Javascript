be2a3aece9dd52b0663e3cc26118333be1783108
unknown <krish.sawant8@gmail.com>
2023-02-13 13:11:03 +0530

    05-Sidebar

---
+++ 05-sidebar/setup/app.js
@@ -0,0 +1,15 @@
+const toggleButton = document.querySelector(".sidebar-toggle");
+const closeButton = document.querySelector(".close-btn");
+const sidebar = document.querySelector(".sidebar");
+
+toggleButton.addEventListener("click", function () {
+  console.log(sidebar.classList);
+  if (sidebar.classList.contains("show-sidebar"))
+    sidebar.classList.remove("show-sidebar");
+  else sidebar.classList.add("show-sidebar");
+});
+
+closeButton.addEventListener("click", function () {
+  sidebar.classList.remove("show-sidebar");
+  console.log(sidebar.classList);
+});
