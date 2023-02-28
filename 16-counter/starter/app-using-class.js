bea12bfdb9e745af96c14e4a31e6830db87ffb4b
unknown <krish.sawant8@gmail.com>
2023-02-28 13:39:45 +0530

    16-counter

---
+++ 16-counter/starter/app-using-class.js
@@ -0,0 +1,56 @@
+// function Counter(element, value) {
+
+// Counter.prototype.increase = function () {};
+
+// Counter.prototype.decrease = function () {
+//   this.value--;
+//   this.valueDOM.textContent = this.value;
+// };
+
+// Counter.prototype.reset = function () {
+//   this.value = this.resetValue;
+//   this.valueDOM.textContent = this.value;
+// };
+
+class Counter {
+  constructor(element, value) {
+    this.counter = document.querySelector(element);
+    this.resetValue = value;
+    this.value = value;
+    this.resetBtn = this.counter.querySelector(".reset");
+    console.log(this.resetBtn);
+    this.increaseBtn = this.counter.querySelector(".increase");
+    this.decreaseBtn = this.counter.querySelector(".decrease");
+    this.valueDOM = this.counter.querySelector(".value");
+    //   console.log(this.valueDOM, this.value);
+    this.valueDOM.textContent = this.value;
+
+    //bind this to all functions
+    this.increase = this.increase.bind(this);
+    this.decrease = this.decrease.bind(this);
+    this.reset = this.reset.bind(this);
+
+    //add event listener to each button
+
+    this.increaseBtn.addEventListener("click", this.increase);
+    this.decreaseBtn.addEventListener("click", this.decrease);
+    this.resetBtn.addEventListener("click", this.reset);
+  }
+
+  increase() {
+    this.value++;
+    this.valueDOM.textContent = this.value;
+  }
+
+  decrease = function () {
+    this.value--;
+    this.valueDOM.textContent = this.value;
+  };
+  reset() {
+    this.value = this.resetValue;
+    this.valueDOM.textContent = this.value;
+  }
+}
+
+const firstCounter = new Counter(".first-counter", 100);
+const secondCounter = new Counter(".second-counter", 200);
